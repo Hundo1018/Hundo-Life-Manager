a286a7213f44a85f82f5bac9e44501f4f7860458
Hundo1018 <f74169@gmail.com>
2023-11-09 02:14:10 +0800

    純純的quill，尚未處理完縮放問題

--- hundo_life_manager/lib/main.dart
+++ hundo_life_manager/lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 import 'package:hundo_life_manager/widgets/note_block.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
 // import 'widgets/traditional_note.dart';
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized(); // 必须确保初始化完成
-  // SharedPreferences prefs = await SharedPreferences.getInstance();
+  //  WidgetsFlutterBinding.ensureInitialized(); // 必须确保初始化完成
+  //  SharedPreferences prefs = await SharedPreferences.getInstance();
 
   // 从SharedPreferences中获取先前保存的数据
-  // String? savedData = prefs.getString('noteData'); // 替换'keyName'为您的键
+  //  String? savedData = prefs.getString('noteData'); // 替换'keyName'为您的键
 
   // runApp(MyApp(savedData ?? "null"));
   runApp(const MyApp("Test"));
@@ -36,15 +38,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title, required this.savedData});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
   final String savedData;
 
@@ -55,18 +48,30 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
+    return QuillProvider(
+        configurations:
+            QuillConfigurations(controller: QuillController.basic()),
+        child: Column(
+          children: [
+            const Text("Note"),
+            const QuillToolbar(
+              configurations: QuillToolbarConfigurations(),
+            ),
+            QuillEditor.basic(
+              configurations: const QuillEditorConfigurations(),
+            )
+          ],
+        ));
+    /*ListView.separated(
       itemBuilder: (BuildContext context, int index) {
         return const NoteBlock();
       },
-      itemCount: 3,
-      padding: const EdgeInsets.symmetric(vertical: 32),
-      shrinkWrap: true,
       separatorBuilder: (BuildContext context, int index) {
         return const Divider(
           color: Colors.blue,
         );
       },
-    );
-      }
+      itemCount: 4,
+    );*/
+  }
 }
